08a074ea16f60ed55780f0071cb13726ad8fa12d
dell <dell@10.30.254.79>
2022-04-15 18:24:04 +0200

    final results

--- gestion_notes/src/gestion_notes/Main.java
+++ gestion_notes/src/gestion_notes/Main.java
@@ -28,7 +28,7 @@ public class Main {
 		mesEtudiants.add(e6);
 		Collections.sort(mesEtudiants);
 		System.out.println(mesEtudiants);
-		System.out.println(mesEtudiants.filtrer(e->e.getNote()<13));
+		System.out.println(mesEtudiants.filtrer(e->e.getNote()>13));
 		System.out.println("les etudiants qui ont le rattrapage :"+mesEtudiants.filtrer(e->e.getNote()<11));
 	}
 	
